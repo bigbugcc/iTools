3f8893bf7db26b1680151be4da225de60ab23a37
bigbug <1373396036@qq.com>
2023-02-13 14:28:41 +0800

    Update Hyperv-Gpu-Vitrual.ps1

--- Hyperv-Gpu-Vitrual.ps1
+++ Hyperv-Gpu-Vitrual.ps1
@@ -22,7 +22,7 @@ Write-Output "3. Open All!"
 function GPU
 {
     Add-VMGpuPartitionAdapter -VMName $vm
-    Set-VMGpuPartitionAdapter -VMName $vm -MinPartitionVRAM 80000000 -MaxPartitionVRAM 100000000 -OptimalPartitionVRAM 100000000 -MinPartitionEncode 80000000 -MaxPartitionEncode 100000000 -OptimalPartitionEncode 100000000 -MinPartitionDecode 80000000 -MaxPartitionDecode 100000000 -OptimalPartitionDecode 100000000 -MinPartitionCompute 80000000 -MaxPartitionCompute 100000000 -OptimalPartitionCompute 100000000
+    Set-VMGpuPartitionAdapter -VMName $vm -MinPartitionVRAM 0 -MaxPartitionVRAM 1000000000 -OptimalPartitionVRAM 1000000000 -MinPartitionEncode 0 -MaxPartitionEncode 18446744073709551615 -OptimalPartitionEncode 18446744073709551615 -MinPartitionDecode 0 -MaxPartitionDecode 1000000000 -OptimalPartitionDecode 1000000000 -MinPartitionCompute 0 -MaxPartitionCompute 1000000000 -OptimalPartitionCompute 1000000000
     Set-VM -GuestControlledCacheTypes $true -VMName $vm
     Set-VM -LowMemoryMappedIoSpace 1Gb -VMName $vm
     Set-VM -HighMemoryMappedIoSpace 12GB -VMName $vm
